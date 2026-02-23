b237b11667c201d2b6de1a9fdfe4aec28ce674b7
Zandolfus <girardiluke@gmail.com>
2026-02-23 03:36:55 -0500

    error 57

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -10,7 +10,6 @@ export const preloadHandlebarsTemplates = async function () {
     'systems/aspects-of-power/templates/actor/parts/actor-items.hbs',
     'systems/aspects-of-power/templates/actor/parts/actor-skills.hbs',
     'systems/aspects-of-power/templates/actor/parts/actor-effects.hbs',
-    'systems/aspects-of-power/templates/actor/parts/actor-stats.hbs',
     // Item partials
     'systems/aspects-of-power/templates/item/parts/item-effects.hbs',
   ]);
